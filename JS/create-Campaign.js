bee0d5de022dafaa0215453579c7773f4cdde9a8
rahiq-h123 <77285448+rahiq-h123@users.noreply.github.com>
2025-05-17 19:52:25 +0300

    Add files via upload

--- JS/create-Campaign.js
+++ JS/create-Campaign.js
@@ -11,61 +11,17 @@ form.addEventListener("submit", function (e) {
   const rewardTitle = document.getElementById('reward-title').value.trim();
   const rewardAmount = parseInt(document.getElementById('reward-amount').value.trim());
   const imageInput = document.getElementById("campaign-img");
-
   const file = imageInput.files[0];
+
   const reader = new FileReader();
 
-  reader.onloadend = function () {
-    const base64Image = reader.result;
+  fetch("http://localhost:3000/campaigns")
+    .then(res => res.json())
+    .then(allCampaigns => {
+      const id = allCampaigns.length + 1;
 
-    fetch("http://localhost:3001/campaigns")
-      .then(res => res.json())
-      .then(allCampaigns => {
-        const id = allCampaigns.length + 1;
-
-        const campaignData = {
-          id,
-          creatorId: currentUser.id,
-          title,
-          description,
-          goal,
-          deadline,
-          isApproved: false,
-          image: base64Image, 
-          rewards: [
-            {
-              id: 5,
-              title: rewardTitle,
-              amount: rewardAmount
-            }
-          ]
-        };
-
-        return fetch("http://localhost:3001/campaigns", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(campaignData)
-        });
-      })
-      .then(res => res.json())
-      .then(data => {
-        window.location.href = 'campaigner-dashboard.html';
-      })
-      .catch(err => {
-        console.error("Erroe in creating campaign", err);
-        alert("An error occured");
-      });
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    fetch("http://localhost:3001/campaigns")
-      .then(res => res.json())
-      .then(allCampaigns => {
-        const id = allCampaigns.length + 1;
+      reader.onloadend = function () {
+        const base64Image = reader.result;
 
         const campaignData = {
           id,
@@ -75,7 +31,7 @@ form.addEventListener("submit", function (e) {
           goal,
           deadline,
           isApproved: false,
-          image: null, 
+          image: base64Image,
           rewards: [
             {
               id: 1,
@@ -85,21 +41,22 @@ form.addEventListener("submit", function (e) {
           ]
         };
 
-        return fetch("http://localhost:3001/campaigns", {
+        fetch("http://localhost:3000/campaigns", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
           body: JSON.stringify(campaignData)
-        });
-      })
-      .then(res => res.json())
-      .then(data => {
-        window.location.href = 'campaigner-dashboard.html';
-      })
-      .catch(err => {
-        console.error("Erroe in creating campaign", err);
-        alert("An rrror occured");
-      });
-  }
+        })
+          .then(() => {
+            window.location.href = 'campaigner-dashboard.html';
+          })
+          .catch(err => {
+            console.error("Error in creating campaign", err);
+            alert("an error occured");
+          });
+      };
+
+      reader.readAsDataURL(file);
+    });
 });
